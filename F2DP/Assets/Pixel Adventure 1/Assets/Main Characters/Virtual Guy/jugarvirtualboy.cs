8089d27385235329cc64f02b05838ed8858a0cca
Emanuel Apaza <emanuel.apaza.200325@gmail.com>
2023-09-19 17:11:52 -0300

    Se logro movimiento de personaje y colliders en tilemaps

--- F2DP/Assets/Pixel Adventure 1/Assets/Main Characters/Virtual Guy/jugarvirtualboy.cs
+++ F2DP/Assets/Pixel Adventure 1/Assets/Main Characters/Virtual Guy/jugarvirtualboy.cs
@@ -4,14 +4,62 @@ using UnityEngine;
 
 public class jugarvirtualboy : MonoBehaviour
 {
+    public float maxspeed = 5f;
     float horizontal, vertical;
-    public float speed=20;
+    public float speed=2600;
+    public Rigidbody2D rig;
+    Animator anim;
+    public bool grounded = false;
+
     // Update is called once per frame
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(rig.velocity.x));
+        anim.SetBool("grounded", grounded);
+       /*horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
-        Vector2 movimiento = ((Vector2.right * horizontal) * (Vector2.up * vertical)) * speed * Time.deltaTime;
-        transform.position += new Vector3 (movimiento.x,movimiento.y,0);
+        Vector2 movimiento = ((Vector2.right * horizontal) + (Vector2.up * vertical)) * speed * Time.deltaTime;
+        rig.velocity += new Vector2(movimiento.x,movimiento.y)*Time.deltaTime;
+        //transform.position += new Vector3 (movimiento.x,movimiento.y,0);*/
+        
+    }
+    void FixedUpdate()
+    {
+        Vector3 fixedVelocity = rig.velocity;
+        fixedVelocity.x *= 0.75f;
+        if (grounded)
+        {
+            rig.velocity = fixedVelocity;
+        }
+        horizontal = Input.GetAxis("Horizontal") * speed;
+        rig.AddForce(Vector2.right * horizontal);
+        float limitedSpeed = Mathf.Clamp(rig.velocity.x, -maxspeed, maxspeed);
+        rig.velocity = new Vector2(limitedSpeed, rig.velocity.y);
+        if (horizontal > 0.1f)
+        {
+            transform.localScale = new Vector3(3.76f, 3.76f, 3.76f);
+        }
+        if (horizontal < -0.1f)
+        {
+            transform.localScale = new Vector3(-3.76f,3.76f, 3.76f);
+        }
+
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Terreno")
+        {
+            Debug.Log("Estoy en el suelo");
+        }
+        if (other.gameObject.tag == "Trofeo")
+        {
+            Debug.Log("Gane un trofeo");
+        }
     }
 }
